7fa86538c4904c6e67d51e619c0e6108a365693f
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-10-26 20:57:49 +0200

    Fixed webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -90,11 +90,7 @@ module.exports = function(webpackEnv) {
       },
       {
         loader: require.resolve('css-loader'),
-        options:{
-          cssOptions,
-          module: true,
-          localIdentName: '[name]__[local]__[hash:base64:5]'
-        },
+        options: cssOptions
       },
       {
         // Options for PostCSS as we reference these options twice
