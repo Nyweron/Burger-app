616d42a93bada53c170421cbd4f06bdf6c4edb29
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-10-26 19:43:04 +0200

    init proj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
