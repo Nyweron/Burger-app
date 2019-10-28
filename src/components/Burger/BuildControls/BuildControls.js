c422de66995eaec2780a750cfbff48d9a9ee2da5
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-10-28 19:03:52 +0100

    Displayed btn's which let add salad, bacon... to burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import classes from './BuildControls.module.css';
+import React from "react";
+import classes from "./BuildControls.module.css";
+import BuildControl from "./BuildControl/BuildControl";
 
-const buildControls = (props) => {
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "backon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" }
+];
+
+const buildControls = props => {
   return (
-    <div>
-
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default buildControls
+export default buildControls;
