24b2ba42a3874585efd5f55c5c9fc3f08bc268ca
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-10-30 11:46:41 +0100

    Added changes, searching bug...

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,28 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../higherOrderComponent/Auxiliary";
 
 const sideDrawer = props => {
-  return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if(props.open){
+    attachedClasses = [classes.sideDrawer, classes.Open];
+  }
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
