2cce98c04574e24d39e03eee710b890d0890f19c
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-11-01 17:10:00 +0100

    Removed console.log

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,8 +2,6 @@ import React from "react";
 import classes from "./Backdrop.module.css";
 
 const backdrop = props => {
-  console.log("props", props);
-
   return props.show ? (
     <div className={classes.Backdrop} onClick={props.clicked}></div>
   ) : null;
