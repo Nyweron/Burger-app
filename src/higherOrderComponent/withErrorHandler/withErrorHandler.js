7d54718679209afff84d206cbcf00ae82e995f43
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-11-01 15:52:04 +0100

    Removed axios.interceptors

--- src/higherOrderComponent/withErrorHandler/withErrorHandler.js
+++ src/higherOrderComponent/withErrorHandler/withErrorHandler.js
@@ -5,19 +5,32 @@ import Aux from "../Auxiliary/Auxiliary";
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
     state = {
       error: null
     };
 
     UNSAFE_componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
 
-      axios.interceptors.response.use(res =>res, error => {
-        this.setState({ error: error });
-      });
+      this.resInterceptor = axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
@@ -29,8 +42,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
         <Aux>
           <Modal
             show={this.state.error}
-            modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null }
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />
         </Aux>
