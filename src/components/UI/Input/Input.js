b781efffd58e16bdb3a04c70053b346a80ce77e1
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-11-05 16:55:06 +0100

    Created dynamic inputs

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import classes from './Input.module.css';
+
+const input = props => {
+  let inputElement = null;
+
+  switch (props.inputtype) {
+    case "input":
+      inputElement = <input className={classes.Input} {...props} />;
+      break;
+    case "textarea":
+      inputElement = <textarea className={classes.Input} {...props} />;
+      break;
+    default:
+      inputElement = <input className={classes.Input} {...props} />;
+  }
+
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+
+export default input;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../components/UI/Button/Button";
 import classes from "./ContactData.module.css";
 import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
@@ -40,7 +41,7 @@ class ContactData extends Component {
       .post("orders", order)
       .then(response => {
         this.setState({ loading: false });
-        this.props.history.push('/');
+        this.props.history.push("/");
       })
       .catch(error => {
         this.setState({ loading: false });
@@ -50,26 +51,26 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="name"
           placeholder="Your name"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="email"
           name="email"
           placeholder="Your email"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="street"
           placeholder="Your street"
         />
-        <input
-          className={classes.Input}
+        <Input
+          inputtype="input"
           type="text"
           name="postal"
           placeholder="Your postalCode"
