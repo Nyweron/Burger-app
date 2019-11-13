7193ba2aa7c489f5f49244411c79b2d18d74a519
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-11-13 12:50:46 +0100

    removed import

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,7 +5,6 @@ import axios from "../../../axios-orders";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 import { connect } from "react-redux";
-import { stat } from "fs";
 
 class ContactData extends Component {
   state = {
