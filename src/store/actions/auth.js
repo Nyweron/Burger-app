3e2ad3f29fcf004a9cba27e69718bb71ad19a420
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-11-19 13:36:36 +0100

    Storing the token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,10 +7,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
   return {
-    type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+      type: actionTypes.AUTH_SUCCESS,
+      idToken: token,
+      userId: userId
   };
 };
 
@@ -42,7 +43,7 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(res => {
         console.log(res);
-        dispatch(authSuccess(res.data));
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
       })
       .catch(err => {
         console.log(err);
