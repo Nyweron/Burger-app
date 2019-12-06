3a4fd38afbc05651024976a950e93aba4ce2a892
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-12-06 18:46:23 +0100

    Logging User out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,9 +9,9 @@ export const authStart = () => {
 
 export const authSuccess = (token, userId) => {
   return {
-      type: actionTypes.AUTH_SUCCESS,
-      idToken: token,
-      userId: userId
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId
   };
 };
 
@@ -19,6 +19,20 @@ export const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
     error: error
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
   };
 };
 
@@ -44,6 +58,7 @@ export const auth = (email, password, isSignup) => {
       .then(res => {
         console.log(res);
         dispatch(authSuccess(res.data.idToken, res.data.localId));
+        dispatch(checkAuthTimeout(res.data.expiresIn));
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error));
