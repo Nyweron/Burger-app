d519f2d05d594cc933dbb2ca142d90facdb4dcf5
Przemysław Juśkiewicz <przemyslaw@pjuskiewicz.com>
2019-12-07 11:44:40 +0100

    Updated UI depends on Auth State

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -12,7 +12,7 @@ const toolbar = props => (
     </div>
 
     <nav className={classes.DesktopOnly}>
-      <NavigationItems />
+      <NavigationItems isAuthenticated={props.isAuth} />
     </nav>
   </header>
 );
--- src/higherOrderComponent/Layout/Layout.js
+++ src/higherOrderComponent/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Aux from "../Auxiliary/Auxiliary";
+import { connect } from 'react-redux';
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
@@ -22,8 +23,11 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
+         isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
         />
@@ -33,4 +37,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+ const mapStateToProps = state => {
+   return {
+    isAuthenticated: state.auth.token !== null
+   };
+ }
+
+export default connect(mapStateToProps)(Layout);
